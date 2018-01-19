68ccb90c17874e370f7e58e05bbc7f8321a8a7df
bpsapient <35607454+bpsapient@users.noreply.github.com>
2018-01-19 12:24:25 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       alert(xhttp.responseText);
+       document.getElementById("mediumDetail").innerHTML = xhttp.responseText;
     }
 };
 xhttp.open("GET", "/res/selectFields.csv", true);
